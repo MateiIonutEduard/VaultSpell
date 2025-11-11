93443d0543f5a5811cd69238d08c81fa3ee41425
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-11-11 12:48:47 +0200

    Adjust the API used for translation between languages to improve efficiency #1
    Adjust the API used for translation between languages to improve efficiency.

--- vault/translator.py
+++ vault/translator.py
@@ -7,18 +7,25 @@ class GoogleTranslate(object):
         self.baseUrl = "https://translate.googleapis.com/translate_a/single?client=gtx"
 
     def GetText(self, text, src, dest):
+        if not text or not text.strip():
+            return text
+            
         url = "{0}&sl={1}&tl={2}&dt=t&q={3}".format(self.baseUrl, src, dest, text)
-        res = req.get(url)
+        try:
+            res = req.get(url)
+            res.raise_for_status()
+            
+            obj = res.json()
 
-        obj = res.json()
+            if obj[0] is None:
+                return text
 
-        if obj[0] is None:
-            return ""
+            result = ''
+            for segment in obj[0]:
+                if segment[0]:
+                    result += segment[0]
 
-        n = len(obj[0])
-        result = ''
-
-        for k in range(n):
-            result += obj[0][k][0]
-
-        return result
+            return result if result else text
+        except Exception as e:
+            print(f"Translation error: {e}")
+            return text
